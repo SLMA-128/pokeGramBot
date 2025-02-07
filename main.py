bf11ad120811ead876b681f2d4c996f5ad211fe2
Sarmiento Leonardo <sarmientolma@gmail.com>
2025-02-06 22:34:10 -0300

    updated profile msg added winrate

--- main.py
+++ main.py
@@ -492,16 +492,23 @@ def profile(message):
         user_data = userEvents.getUserByName(username)
         victories = user_data.get("victories", [])
         defeats = user_data.get("defeats", [])
-        # Formatear victorias y derrotas
-        victories_text = ",\n\U0001F539".join([f"{opponent}: {count}" for entry in victories for opponent, count in entry.items()]) if victories else "None"
-        defeats_text = ",\n\U0001F538".join([f"{opponent}: {count}" for entry in defeats for opponent, count in entry.items()]) if defeats else "None"
+        total_victories = sum(entry["count"] for entry in victories) if victories else 0
+        total_defeats = sum(entry["count"] for entry in defeats) if defeats else 0
+        victories_text = "\n\U0001F539 " + "\n\U0001F539 ".join(
+            [f"{entry['opponent']}: {entry['count']}" for entry in victories]
+        ) if victories else "None"
+        defeats_text = "\n\U0001F538 " + "\n\U0001F538 ".join(
+            [f"{entry['opponent']}: {entry['count']}" for entry in defeats]
+        ) if defeats else "None"
+        winrate = round((total_victories / (total_victories + total_defeats)) * 100, 2) if (total_victories + total_defeats) > 0 else 0
         # Mensaje de respuesta
         profile_text = (
             f"\U0001F4DC *{user_data["name"]} Profile*\n"
             f"\U0001F4E6 Pokemons Captured: {user_data["total_pokemons"]}\n"
             f"\U0001F31F Shiny Captured: {user_data["total_shiny"]}\n"
-            f"\U0001F3C6 Victories:\nTotal: {len(victories) if not None else 0}\n{victories_text}\n"
-            f"\U0001F480 Defeats:\nTotal: {len(defeats) if not None else 0}\n{defeats_text}"
+            f"\U0001F3AF Winrate: {winrate}%\n"
+            f"\U0001F3C6 Victories:\nTotal: {total_victories}\n{victories_text}\n"
+            f"\U0001F480 Defeats:\nTotal: {total_defeats}\n{defeats_text}"
         )
         msg = bot.reply_to(message, profile_text, parse_mode="Markdown")
         threading.Timer(30, lambda: bot.delete_message(chat_id=message.chat.id, message_id=msg.message_id)).start()
